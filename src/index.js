344e00d0e0f8f1dfe937e9812055e568c41d0e92
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-05-23 21:21:24 +0530

    adds dev tools to the mobx state tree project

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+import { onPatch } from "mobx-state-tree";
+import makeInspectable from "mobx-devtools-mst";
+
 import Invoice from "./models/Invoice";
 
 const invoice = Invoice.create({ currency: "INR" });
+
+// method one --> prints in console
+onPatch(invoice, patch => {
+  console.log(patch);
+});
+
+// method two --> mobx dev tools extension
+makeInspectable(invoice);
 
 ReactDOM.render(<App invoice={invoice} />, document.getElementById("root"));
